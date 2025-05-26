e985b1836392dfa0b448173668aec355cce01501
Timothy Paul Read <40046302+gaialive@users.noreply.github.com>
2025-05-27 00:40:26 +0100

    Add files via upload
    4th part of documentation

---
+++ docs/struct_midi_note_message.js
@@ -0,0 +1,6 @@
+var struct_midi_note_message =
+[
+    [ "noteNumber", "struct_midi_note_message.html#a218b5b65965ff912083b59a83d974e8d", null ],
+    [ "timestamp", "struct_midi_note_message.html#a939ab73ed422544e5b025eb0931a650c", null ],
+    [ "velocity", "struct_midi_note_message.html#aa5eef7408f0e44088f27cd223d46a818", null ]
+];
---
+++ docs/zdf__render_8cpp.js
@@ -0,0 +1,12 @@
+var zdf__render_8cpp =
+[
+    [ "cleanup", "zdf__render_8cpp.html#a92746fbd3c3b36f88482f85aeb6cdfa0", null ],
+    [ "render", "zdf__render_8cpp.html#a248ba047ea7cef8648bdcc3fee51a156", null ],
+    [ "setup", "zdf__render_8cpp.html#a2f5460cb50379a189a77c3dddd03384d", null ],
+    [ "frequency", "zdf__render_8cpp.html#acdfc8898c9e67fbcec81f3b04ae61bd9", null ],
+    [ "lfoFreq", "zdf__render_8cpp.html#a32162205185394ca30b85177bf49acdc", null ],
+    [ "lfoPhase", "zdf__render_8cpp.html#a3234f1525ec0e37ad49148e9e2fd654d", null ],
+    [ "phase", "zdf__render_8cpp.html#a275dd6dafe5d4455e599ffd15feb06f8", null ],
+    [ "phaseIncrement", "zdf__render_8cpp.html#adc2c35006ba5584262de7ab2aa5d0b17", null ],
+    [ "zdfMLFilter", "zdf__render_8cpp.html#a9a26a1d3e052b53f64cc294ec2abbb83", null ]
+];
